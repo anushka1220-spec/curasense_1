4334443e930cb48a24e64f0c62cd74b5d2d1ff7a
Your Name <your.email@example.com>
2026-03-31 01:05:51 +0530

    Update backend - fixed views import and added Flask setup

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask
+from views import views
 
 app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+app.register_blueprint(views , url_prefix="/views")
 
 if __name__ == "__main__":
    app.run(debug=True, port=8000)
